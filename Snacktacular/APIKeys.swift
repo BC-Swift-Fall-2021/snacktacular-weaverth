e12256c018b58c6cbbaafd84769276c10d8be542
weaverth <weaverth@bc.edu>
2021-11-01 15:39:14 -0400

    8.7 Done!

---
+++ Snacktacular/APIKeys.swift
@@ -0,0 +1,12 @@
+//
+//  APIKeys.swift
+//  Snacktacular
+//
+//  Created by Teddy Weaver on 11/1/21.
+//
+
+import Foundation
+
+struct APIKeys{
+    static let googlePlacesKey = "AIzaSyAtW0CqiN9J85FgNvQzJZGcfACEQ_0f-r8"
+}
